c6eba2fbc1cfb3641cd3600ff60b09f8df2f33e4
Fikri Rakala <fikrirakala@gmail.com>
2025-01-23 07:42:34 +0800

    Create Header Component

--- src/app/(home)/hero.tsx
+++ src/app/(home)/hero.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import Header from "@/components/header";
 import { Button } from "@/components/ui/button";
 
 export default function Hero() {
@@ -8,7 +9,8 @@ export default function Hero() {
       <div className="rounded-4xl absolute inset-2 bottom-0 bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-[28%] via-[#ee87cb] via-[70%] to-[#b060ff] ring-1 ring-inset ring-black/5 sm:bg-[linear-gradient(145deg,var(--tw-gradient-stops))]"></div>
 
       <div className="relative px-6 lg:px-8">
-        <div className="mx-auto max-w-7xl">
+        <div className="mx-auto max-w-2xl lg:max-w-7xl">
+          <Header />
           <div className="pb-24 pt-16 sm:pb-32 sm:pt-24 md:pb-48 md:pt-32">
             <h1 className="font-display text-balance text-6xl/[0.9] font-medium tracking-tight sm:text-8xl/[0.8] md:text-9xl/[0.8]">
               Close every deal.
---
+++ src/components/header.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import Logo from "../../public/logo.svg";
+
+const links = [
+  {
+    title: "Pricing",
+    href: "/",
+  },
+  {
+    title: "Company",
+    href: "/",
+  },
+  {
+    title: "Blog",
+    href: "/",
+  },
+  {
+    title: "Login",
+    href: "/",
+  },
+];
+
+export default function Header() {
+  return (
+    <header className="pt-12 sm:pt-16">
+      <div className="group/row relative isolate flex justify-between pt-[calc(theme(spacing.2)+1px)] last:pb-[calc(theme(spacing.2)+1px)]">
+        <div
+          aria-hidden="true"
+          className="absolute inset-y-0 left-1/2 -z-10 w-screen -translate-x-1/2"
+        >
+          <div className="absolute inset-x-0 top-0 border-t border-black/5"></div>
+          <div className="absolute inset-x-0 top-2 border-t border-black/5"></div>
+          <div className="absolute inset-x-0 bottom-0 hidden border-b border-black/5 group-last/row:block"></div>
+          <div className="absolute inset-x-0 bottom-2 hidden border-b border-black/5 group-last/row:block"></div>
+        </div>
+
+        <div className="py-3">
+          <Link href="/">
+            <Image src={Logo} alt="" className="h-9 overflow-visible" />
+          </Link>
+        </div>
+
+        <nav className="relative hidden lg:flex">
+          {links.map((link) => (
+            <Link
+              key={link.title}
+              href={link.href}
+              className="flex items-center px-4 py-3 text-base font-medium text-gray-950 bg-blend-multiply hover:bg-black/[2.5%]"
+            >
+              {link.title}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </header>
+  );
+}
