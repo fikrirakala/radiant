cfa16868fc9794fd2fd94d19f60c088114e0ab39
Fikri Rakala <fikrirakala@gmail.com>
2025-01-22 08:42:54 +0800

    Install Shadcn/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,6 @@ import "./globals.css";
 const geistSans = localFont({
   src: "../assets/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "../assets/fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
 });
 
 export const metadata: Metadata = {
@@ -25,12 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" suppressHydrationWarning className={geistSans.className}>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
