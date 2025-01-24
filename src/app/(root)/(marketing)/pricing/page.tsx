f8f8cf55da98c72c09c2e2ae7ca7c3eb740b2f15
Fikri Rakala <fikrirakala@gmail.com>
2025-01-24 20:17:38 +0800

    Create Testimonial Section of Pricing Page

--- src/app/(root)/(marketing)/pricing/page.tsx
+++ src/app/(root)/(marketing)/pricing/page.tsx
@@ -1,12 +1,15 @@
 import FaqSection from "./faq-section";
 import Hero from "./hero";
 import PricingSection from "./pricing";
+import TestimonialSection from "./testimonial-section";
 
 export default function PricingPage() {
   return (
     <>
       <Hero />
       <PricingSection />
+      <div className="h-[50vh]"></div>
+      <TestimonialSection />
       <FaqSection />
     </>
   );
